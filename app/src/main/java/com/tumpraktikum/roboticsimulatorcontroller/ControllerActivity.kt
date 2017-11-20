22fa56113887f8ab2d1598c5757edfa3cfae1484
Hannes <h.oberprantacher@gmail.com>
2017-11-20 23:33:53 +0100

    Controller added

---
+++ app/src/main/java/com/tumpraktikum/roboticsimulatorcontroller/ControllerActivity.kt
@@ -0,0 +1,12 @@
+package com.tumpraktikum.roboticsimulatorcontroller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ControllerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_controller)
+    }
+}
--- app/src/main/java/com/tumpraktikum/roboticsimulatorcontroller/MainActivity.kt
+++ app/src/main/java/com/tumpraktikum/roboticsimulatorcontroller/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.tumpraktikum.roboticsimulatorcontroller
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickListener = View.OnClickListener { view ->
+            when (view.id) {
+                R.id.btnConnect -> openControllerActivity()
+            }
+        }
+
+        btnConnect.setOnClickListener(clickListener)
+    }
+
+    private fun openControllerActivity() {
+        // Create an Intent to start the second activity
+        val intent = Intent(this, ControllerActivity::class.java)
+
+        // Start the new activity.
+        startActivity(intent)
     }
 }
