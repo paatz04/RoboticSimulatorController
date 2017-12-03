90d59fbd94fe62bd304bb4a959fbadfd6b60c348
Patric Corletto <patric_corletto@hotmail.com>
2017-12-03 23:18:41 +0100

    replace listener with kotlin lambda function :)

--- app/src/main/java/com/tumpraktikum/roboticsimulatorcontroller/MainActivity.kt
+++ app/src/main/java/com/tumpraktikum/roboticsimulatorcontroller/MainActivity.kt
@@ -3,7 +3,6 @@ package com.tumpraktikum.roboticsimulatorcontroller
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,19 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val clickListener = View.OnClickListener { view ->
-            when (view.id) {
-                R.id.btnConnect -> openControllerActivity()
-            }
-        }
-
-        btnConnect.setOnClickListener(clickListener)
+        btnConnect.setOnClickListener{openControllerActivity()}
     }
 
     private fun openControllerActivity() {
         // Create an Intent to start the second activity
         val intent = Intent(this, ControllerActivity::class.java)
-
         // Start the new activity.
         startActivity(intent)
     }
